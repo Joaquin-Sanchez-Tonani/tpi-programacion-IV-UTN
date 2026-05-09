06ac9cafbf24a93819020b727efd38dfa27255cc
WeFake123 <augustoagi12345@gmail.com>
2026-05-09 16:47:53 -0300

    Fix repository structure

---
+++ Src/Presentation/Controller/UserChild/AdminController.cs
@@ -0,0 +1,15 @@
+﻿using Application.Services;
+using Domain.Entity.UsersChild;
+
+namespace Presentation.Controller.UserChild
+{
+
+
+    public class AdminController : UsersController<Admin>
+    {
+        public AdminController(IUserService service) : base(service)
+        {
+            // Aquí puedes agregar métodos que SOLO existan para admins
+        }
+    }
+}
---
+++ Src/Presentation/Program.cs
@@ -0,0 +1,58 @@
+using Application.Services;
+using Application.Services.UsersChild;
+using Application.Tools;
+using Domain.Interface;
+using Domain.Interface.UsersInterface;
+using Infrastructure.Repositories.UsersChild;
+using Infrastructure.Repositories.UsersChild.cs;
+using Infrastructure.Service;
+using Microsoft.EntityFrameworkCore;
+using Trabajop4.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// --- CONFIGURACIÓN DE SERVICIOS ---
+
+builder.Services.AddControllers();
+
+// Swagger / OpenAPI
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// 1. Capa de Infraestructura (Persistencia)
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+   options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+// 2. Repositorios
+// Es vital registrar IUserRepository porque UserService lo pide en su constructor
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IClientRepository, ClientRepository>();
+builder.Services.AddScoped<IAdminRepository, AdminRepository>();
+builder.Services.AddScoped<ISysAdminRepository, SysAdminRepository>();
+
+
+// 3. Servicios de Aplicación (Lógica de Negocio)
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IClientService, ClientService>();
+builder.Services.AddScoped<IAdminService, AdminService>();
+
+builder.Services.AddScoped<IPasswordHasherService, PasswordHasherService>();
+
+// --- PIPELINE DE LA APLICACIÓN ---
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
