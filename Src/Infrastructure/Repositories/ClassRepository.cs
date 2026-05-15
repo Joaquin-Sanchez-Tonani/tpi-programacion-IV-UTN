e8466b321b9c1686a10ecd08ac1b80f22ef0d16b
Maximo <maxihahn0@gmail.com>
2026-05-15 18:56:21 -0300

    Complete implementation of all IRepository, Repository, IService, and Service classes

---
+++ Src/Infrastructure/Repositories/ClassRepository.cs
@@ -0,0 +1,47 @@
+﻿using Domain.Entity;
+using Domain.Interface;
+using Microsoft.EntityFrameworkCore;
+using Trabajop4.Infrastructure;
+
+namespace Infrastructure
+{
+    public class ClassRepository : IClassRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClassRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Class>> GetAll()
+        {
+            return await _context.Classes.ToListAsync();
+        }
+
+        public async Task<Class?> GetById(Guid id)
+        {
+            return await _context.Classes.FindAsync(id);
+        }
+
+        public async Task Add(Class gymClass)
+        {
+            await _context.Classes.AddAsync(gymClass);
+        }
+
+        public async Task Update(Class gymClass)
+        {
+            _context.Classes.Update(gymClass);
+        }
+
+        public async Task Delete(Class gymClass)
+        {
+            _context.Classes.Remove(gymClass);
+        }
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
